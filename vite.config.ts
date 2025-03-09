4546845f0d61c91ed1745d0f9a2ff541db22dc8d
Jahye Hwang <meltinp@gmail.com>
2025-03-09 18:46:23 +0900

    Add GitHub Actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/my-blog/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
